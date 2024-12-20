9d8d781f7538e42979774396691b8009a75a70aa
Satveer Singh <satveersingh0905@gmail.com>
2024-12-20 14:16:09 +0530

    improvised imbedded repository

---
+++ fl2/JavaScript/4.js
@@ -0,0 +1,8 @@
+// Importing specific functions and constants from 3.js
+import { add, subtract, PI, multiply } from './3.js';
+
+// Using imported functions and constants
+console.log("add:", add(5, 3));           // Output: add: 8
+console.log("subtract:", subtract(5, 3)); // Output: subtract: 2
+console.log("PI:", PI);                   // Output: PI: 3.14159
+console.log("multiply:", multiply(5, 3)); // Output: multiply: 15
